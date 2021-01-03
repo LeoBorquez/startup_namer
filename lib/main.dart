5e9f49c1e78873cfe1f1d2a4c22ecfcc0844cbd3
Leo Borquez <leoborquezg@protonmail.com>
2021-01-03 00:49:03 -0300

    🚀 RELEASE:

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,46 @@
-import 'dart:js';
-
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
+// #docregion MyApp
 class MyApp extends StatelessWidget {
+  // #docregion build
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'tab tittle',
-      theme: ThemeData(
-        primaryColor: Colors.white,
-      ),
+      title: 'Startup Name Generator',
       home: RandomWords(),
     );
   }
+  // #enddocregion build
 }
+// #enddocregion MyApp
 
-class RandomWords extends StatefulWidget {
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
+// #docregion RWS-var
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  final _saved = Set<WordPair>();
+  final _saved = <WordPair>{};
   final _biggerFont = TextStyle(fontSize: 18.0);
+  // #enddocregion RWS-var
 
+  // #docregion _buildSuggestions
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if (i.isOdd) return Divider();
-        final index = i ~/ 2;
-        if (index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      },
-    );
-  }
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
 
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+  // #enddocregion _buildSuggestions
+
+  // #docregion _buildRow
   Widget _buildRow(WordPair pair) {
     final alreadySaved = _saved.contains(pair);
     return ListTile(
@@ -64,20 +63,26 @@ class _RandomWordsState extends State<RandomWords> {
       },
     );
   }
+  // #enddocregion _buildRow
 
+  // #docregion RWS-build
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
-        actions: [IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)],
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
   }
+  // #enddocregion RWS-build
 
   void _pushSaved() {
     Navigator.of(context).push(
+      // Add lines from here...
       MaterialPageRoute<void>(
         builder: (BuildContext context) {
           final tiles = _saved.map(
@@ -102,7 +107,14 @@ class _RandomWordsState extends State<RandomWords> {
             body: ListView(children: divided),
           );
         },
-      ),
+      ), // ...to here.
     );
   }
+  // #docregion RWS-var
+}
+// #enddocregion RWS-var
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
 }
